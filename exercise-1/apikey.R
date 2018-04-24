09907a172064f57b689b2ee818ed3a67917d897b
Abraham Lee <abelee2016@gmail.com>
2018-04-23 17:08:43 -0700

    exercise-1

---
+++ exercise-1/apikey.R
@@ -0,0 +1,1 @@
+nyt_apikey <-"7c9ac09fb44f4c07ba18b45cc7e38c4a"
--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,13 +1,15 @@
+# Exercise 2: working with data APIs
+
 # load relevant libraries
 library(httr)
 library(jsonlite)
 
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
-
+source("apikey.R")
 
 # Create a variable `movie.name` that is the name of a movie of your choice.
-
+movie_name <- "Interstellar"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -17,27 +19,34 @@ library(jsonlite)
 #
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie.name` variable as the search query!
-
+base_uri <- "https://api.nytimes.com/svc/movies/v2/"
+resource <- "reviews/search.json"
+query_params <- list("api-key" = nyt_apikey, query = movie_name)
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
-
+query <- GET(paste0(base_uri, resource), query = query_params)
+search_result <- fromJSON(content(query, "text"))
 
 # What kind of data structure did this produce? A data frame? A list?
-
+# A list
 
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
 # Use functions such as `names()`, `str()`, etc.
+movie_returned <- names(search_result)
+reviews <- names(search_result$results)
 
 
 # Flatten the movie reviews content into a data structure called `reviews`
-
+reviews <- flatten(search_result$results)
 
 # From the most recent review, store the headline, short summary, and link to
 # the full article, each in their own variables
-
+headline <- reviews$headline
+short_summary <- reviews$summary_short
+link <- reviews$link.url
 
 # Create a list of the three pieces of information from above. 
 # Print out the list.
-
+print(list(headline, short_summary, link))
